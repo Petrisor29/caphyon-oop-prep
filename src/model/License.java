d68ad7332a018b51b57002f7d042720da87e6f4f
Petrisor29 <petrisor.gnr@gmail.com>
2025-11-14 20:53:57 +0200

    Add License class with constructor and toString method

--- src/model/License.java
+++ src/model/License.java
@@ -1,1 +1,19 @@
+public class License{
+  private String key;
+  private String expiryDate;
+  private Product product;
 
+  public License(String key, String expiryDate, Product product){
+    this.key = key;
+    this.expiryDate = expiryDate;
+    this.product = product;
+  }
+
+  @Override
+  public String toString(){
+    return "License key: " + key + "\n"
+       + "Expiry date: " + expiryDate + "\n"
+       + "Product: " + product
+       + "\n"  ;
+  }
+}
