8a5d27a98884778c86b34f76c46feed523b886d8
Petrisor29 <petrisor.gnr@gmail.com>
2025-11-14 20:55:18 +0200

    Add product, license, and user instances

--- src/model/Main.java
+++ src/model/Main.java
@@ -1,1 +1,8 @@
+Product laptop = new Product("12345", "Laptop", "1.0", 1000.00");
+License licenta = new License("123456789", "2023-12-31", Laptop);
+User john = new User("John Doe", "john.doe@example.com", license);
+
+System.out.println(Laptop);
+System.out.println(Licenta);
+System.out.println(John);
 
